a4c2acec0ca2227e60d69dec7b9a919414c1a2a3
Raven Best <raven.best5@gmail.com>
2018-09-08 17:09:21 -0400

    Assignment Bloc Jams Completed

--- scripts/helper.js
+++ scripts/helper.js
@@ -2,13 +2,13 @@ class Helper {
   prettyTime(timeInSeconds) {
     const min = Math.floor(timeInSeconds / 60);
     const sec = Math.floor(timeInSeconds - min * 60);
-      return ${min}:${sec};
+      return `${min}:${sec}`;
   };
 
   playPauseAndUpdate(song) {
     player.playPause(song);
-    const duration = player.getDuration();
-    $('#time-control .total-time').text(duration);
+    const duration = player.currentlyPlaying.duration;
+    $('#time-control .total-time').text(this.prettyTime(duration));
   };
 };
 
